5316162f7a0bdb2b8c721c3c7602a3ecd2c0322c
Daizu <daizutabi@gmail.com>
2025-04-18 09:16:07 +0000

    Update dependencies and refactor markdown parsing functions; remove obsolete tests and notebooks

--- src/nbsync/markdown.py
+++ src/nbsync/markdown.py
@@ -12,14 +12,31 @@ if TYPE_CHECKING:
 Element: TypeAlias = str | CodeBlock | Image
 
 
-def parse(text: str) -> Iterator[Element]:
-    elems = nbstore.markdown.parse(text)
+def parse_image(image: Image) -> Iterator[str | Image | CodeBlock]:
+    if image.source:
+        image.identifier = image.identifier or str(uuid.uuid4())
+        yield CodeBlock("", image.identifier, [], {}, image.source, image.url)
+        yield image
 
-    for elem in parse_url(elems):
-        if isinstance(elem, Image):
-            yield from parse_image(elem)
-        else:
-            yield elem
+    elif image.identifier:
+        yield image
+
+    else:
+        yield image.text
+
+
+SUPPORTED_EXTENSIONS = (".ipynb", ".md", ".py")
+
+
+def set_url(elem: Image | CodeBlock, url: str) -> tuple[Element, str]:
+    if elem.url in ["", "."] and url:
+        elem.url = url
+        return elem, url
+
+    if elem.url.endswith(SUPPORTED_EXTENSIONS):
+        return elem, elem.url
+
+    return elem.text, url
 
 
 def parse_url(elems: Iterable[Element]) -> Iterator[Element]:
@@ -37,28 +54,11 @@ def parse_url(elems: Iterable[Element]) -> Iterator[Element]:
             yield elem
 
 
-SUPPORTED_EXTENSIONS = (".ipynb", ".md", ".py")
+def parse(text: str) -> Iterator[Element]:
+    elems = nbstore.markdown.parse(text)
 
-
-def set_url(elem: Image | CodeBlock, url: str) -> tuple[Element, str]:
-    if elem.url in ["", "."] and url:
-        elem.url = url
-        return elem, url
-
-    if elem.url.endswith(SUPPORTED_EXTENSIONS):
-        return elem, elem.url
-
-    return elem.text, url
-
-
-def parse_image(image: Image) -> Iterator[str | Image | CodeBlock]:
-    if image.source:
-        image.identifier = image.identifier or str(uuid.uuid4())
-        yield CodeBlock("", image.identifier, [], {}, image.source, image.url)
-        yield image
-
-    elif image.identifier:
-        yield image
-
-    else:
-        yield image.text
+    for elem in parse_url(elems):
+        if isinstance(elem, Image):
+            yield from parse_image(elem)
+        else:
+            yield elem
---
+++ tests/test_markdown.py
@@ -0,0 +1,191 @@
+import pytest
+from nbstore.markdown import CodeBlock, Image, parse
+
+
+def test_parse_image():
+    from nbsync.markdown import parse_image
+
+    text = "![a](b.ipynb){#c}"
+    image = next(parse(text))
+    assert isinstance(image, Image)
+    image = next(parse_image(image))
+    assert isinstance(image, Image)
+    assert image.alt == "a"
+    assert image.url == "b.ipynb"
+    assert image.identifier == "c"
+
+
+def test_parse_image_no_identifier():
+    from nbsync.markdown import parse_image
+
+    text = "![ a ]( b.ipynb ){  c ' b ' }"
+    image = next(parse(text))
+    assert isinstance(image, Image)
+    text_ = next(parse_image(image))
+    assert isinstance(text_, str)
+    assert text_ == text
+
+
+def test_parse_image_source_with_identifier():
+    from nbsync.markdown import parse_image
+
+    text = '![a](b.ipynb){#c `x=1` exec="1"}'
+    image = next(parse(text))
+    assert isinstance(image, Image)
+    assert image.source
+    it = parse_image(image)
+    code_block = next(it)
+    assert isinstance(code_block, CodeBlock)
+    assert code_block.text == ""
+    assert code_block.url == "b.ipynb"
+    assert code_block.identifier == "c"
+    assert code_block.classes == []
+    assert code_block.attributes == {}
+
+    image = next(it)
+    assert isinstance(image, Image)
+    assert image.text == text
+    assert image.url == "b.ipynb"
+    assert image.identifier == "c"
+    assert image.classes == []
+    assert image.attributes == {"exec": "1"}
+
+
+def test_parse_image_source_without_identifier():
+    from nbsync.markdown import parse_image
+
+    text = '![a](b.ipynb){`x=1` exec="1"}'
+    image = next(parse(text))
+    assert isinstance(image, Image)
+    it = parse_image(image)
+    code_block = next(it)
+    image = next(it)
+    assert isinstance(code_block, CodeBlock)
+    assert isinstance(image, Image)
+    assert code_block.identifier == image.identifier
+
+
+def test_set_url():
+    from nbsync.markdown import set_url
+
+    image = Image("", "a", [], {}, "", "b.ipynb")
+    image, url = set_url(image, "")
+    assert isinstance(image, Image)
+    assert url == "b.ipynb"
+
+
+def test_set_url_not_supported_extension():
+    from nbsync.markdown import set_url
+
+    image = Image("abc", "a", [], {}, "", "b.txt")
+    text, url = set_url(image, "a.ipynb")
+    assert text == "abc"
+    assert url == "a.ipynb"
+
+
+@pytest.mark.parametrize("url", ["", "."])
+def test_set_url_empty_url(url: str):
+    from nbsync.markdown import set_url
+
+    image = Image("abc", "a", [], {}, "", url)
+    image, url = set_url(image, "a.ipynb")
+    assert isinstance(image, Image)
+    assert image.url == "a.ipynb"
+    assert url == "a.ipynb"
+
+
+def test_parse_url():
+    from nbsync.markdown import parse_url
+
+    images = [
+        Image("abc", "a", [], {}, "", "a.py"),
+        Image("abc", "a", [], {}, "", ""),
+        Image("abc", "a", [], {}, "", "."),
+    ]
+    images = list(parse_url(images))
+    for image in images:
+        assert isinstance(image, Image)
+        assert image.url == "a.py"
+
+
+def test_parse_url_code_block():
+    from nbsync.markdown import parse_url
+
+    code_blocks = [CodeBlock("abc", "a", [], {}, "", "")]
+    text = list(parse_url(code_blocks))[0]
+    assert text == "abc"
+
+
+def test_parse_url_str():
+    from nbsync.markdown import parse_url
+
+    text = list(parse_url(["abc"]))[0]
+    assert text == "abc"
+
+
+SOURCE = """\
+![alt](a.py){#_}
+![alt](){#id}
+```python
+a
+```
+```python {.#id2}
+b
+```
+![alt](){`x=1`}
+"""
+
+
+@pytest.fixture(scope="module")
+def elems():
+    from nbsync.markdown import parse
+
+    return list(parse(SOURCE))
+
+
+def test_len(elems):
+    assert len(elems) == 11
+
+
+def test_elems_0(elems):
+    image = elems[0]
+    assert isinstance(image, Image)
+    assert image.identifier == "_"
+    assert image.url == "a.py"
+
+
+def test_elems_2(elems):
+    image = elems[2]
+    assert isinstance(image, Image)
+    assert image.url == "a.py"
+    assert image.identifier == "id"
+
+
+def test_elems_4(elems):
+    assert elems[4] == "```python\na\n```"
+
+
+def test_elems_6(elems):
+    code_block = elems[6]
+    assert isinstance(code_block, CodeBlock)
+    assert code_block.url == "a.py"
+    assert code_block.identifier == "id2"
+    assert code_block.source == "b"
+
+
+def test_elems_8(elems):
+    code_block = elems[8]
+    assert isinstance(code_block, CodeBlock)
+    assert code_block.url == "a.py"
+    assert code_block.source == "x=1"
+
+
+def test_elems_9(elems):
+    image = elems[9]
+    assert isinstance(image, Image)
+    assert image.url == "a.py"
+    assert image.identifier == elems[8].identifier
+
+
+def test_elems_10(elems):
+    assert elems[10] == "\n"
